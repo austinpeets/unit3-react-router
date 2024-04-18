bc58fc3433a1d7fe8acd2397355c485c0e94f7c5
Austin Pitts <austinpitts@Austins-Laptop.local>
2024-04-17 23:27:30 -0400

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
+import Home from './Components/Home'
+import Blue from './Components/Blue'
+import Red from './Components/Red'
 import './App.css'
+
 
 function App() {
 
@@ -10,13 +14,15 @@ function App() {
       <div id="container">
       <h1>Hello React Router!</h1>
       <div id="navbar">
+        <Link to='/home'>Home</Link>
         <Link to='/blue'>Blue</Link>
         <Link to='/red'>Red</Link>
       </div>
       <div id="main-section">
         <Routes>
-          <Route path='/blue' element={<h1>Blue</h1>}></Route>
-          <Route path='/red' element={<h1>Red</h1>}></Route>
+          <Route path='/home' element={<Home />}></Route>
+          <Route path='/blue' element={<Blue />}></Route>
+          <Route path='/red' element={<Red />}></Route>
         </Routes>
       </div>
     </div>
